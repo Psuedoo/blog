f89acef20b1f981da6752421e09308f4dc226e3c
Psuedoo <forthewin720@gmail.com>
2025-09-26 16:27:06 -0400

    update base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://psuedoo.github.io",
   // Only use base path in production (GitHub Pages)
-  base: process.env.NODE_ENV === "production" ? "/astro-theme-terminal" : "/",
+  base: process.env.NODE_ENV === "production" ? "/blog" : "/",
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
